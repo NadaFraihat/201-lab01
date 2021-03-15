0d0890cad28786e9afa73340759e2d5bd9d4ee40
NadaFraihat <nada997fraihat@gmail.com>
2021-03-15 14:42:39 +0200

    edit

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,15 @@ case 'yes':
 //console.log('Sorry, but I hate and scared of it! I wish one day I Can face this thing');
   alert('Sorry, but I hate and scared of it! I wish one day I Can face this thing');
   break;
+case 'y':
+//console.log('Sorry, but I hate and scared of it! I wish one day I Can face this thing');
+  alert('Sorry, but I hate and scared of it! I wish one day I Can face this thing');
+  break;
 case 'no':
+//console.log('WOW! How did you know my secret!');
+  alert('WOW! How did you know my secret!');
+  break;
+case 'n':
 //console.log('WOW! How did you know my secret!');
   alert('WOW! How did you know my secret!');
   break;
@@ -22,7 +30,15 @@ case 'yes':
 //console.log('Oh! It is my big big dream! I wish I can travel all around the world');
   alert('Oh! It is my big big dream! I wish I can travel all around the world');
   break;
+case 'y':
+//console.log('Oh! It is my big big dream! I wish I can travel all around the world');
+  alert('Oh! It is my big big dream! I wish I can travel all around the world');
+  break;
 case 'no':
+//console.log('Unfortunately.');
+  alert('Unfortunately.');
+  break;
+case 'n':
 //console.log('Unfortunately.');
   alert('Unfortunately.');
   break;
@@ -36,7 +52,15 @@ case 'yes':
 //console.log('No! Not because I sraced. But I dont get any benifits from it! except sleeping with light On');
   alert('No! Not because I sraced. But I dont get any benifits from it! except sleeping with light On');
   break;
+case 'y':
+//console.log('No! Not because I sraced. But I dont get any benifits from it! except sleeping with light On');
+  alert('No! Not because I sraced. But I dont get any benifits from it! except sleeping with light On');
+  break;
 case 'no':
+//console.log('hooorry!');
+  alert('hooorry!');
+  break;
+case 'n':
 //console.log('hooorry!');
   alert('hooorry!');
   break;
@@ -50,7 +74,15 @@ case 'yes':
 //console.log('Yes!!! such I am so intrested to know about you my new friend');
   alert('Yes!!! such I am so intrested to know about you my new friend');
   break;
+case 'y':
+//console.log('Yes!!! such I am so intrested to know about you my new friend');
+  alert('Yes!!! such I am so intrested to know about you my new friend');
+  break;
 case 'no':
+//console.log('I hate being lonely ');
+  alert('I hate being lonely  ');
+  break;
+case 'n':
 //console.log('I hate being lonely ');
   alert('I hate being lonely  ');
   break;
@@ -64,7 +96,15 @@ case 'yes':
 //console.log('lol! It is fine It does not matter ');
   alert('lol! It is fine It does not matter ');
   break;
+case 'y':
+//console.log('lol! It is fine It does not matter ');
+  alert('lol! It is fine It does not matter ');
+  break;
 case 'no':
+//console.log('Oh! Where did you hear this? It is a Rumours ');
+  alert('Oh! Where did you hear this? It is a Rumours');
+  break;
+case 'n':
 //console.log('Oh! Where did you hear this? It is a Rumours ');
   alert('Oh! Where did you hear this? It is a Rumours');
   break;
