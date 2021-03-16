e5fe8ca16f799dfd0cdd115bcb4226f1c338dfa5
NadaFraihat <nada997fraihat@gmail.com>
2021-03-16 15:57:08 +0200

    msg

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 'use strict';
+let score=0;
 let userName= prompt('Enter your name, Please!');
 alert('Welcome '+ userName+' to my new web!');
 let activityCase= prompt('Do I like swimming?');
@@ -14,16 +15,19 @@ case 'y':
 case 'no':
 //console.log('WOW! How did you know my secret!');
   alert('WOW! How did you know my secret!');
+  score++;
   break;
 case 'n':
 //console.log('WOW! How did you know my secret!');
   alert('WOW! How did you know my secret!');
+  score++;
   break;
 default:
   alert('Why! try it! Just guess yes or no ')
   activityCase= prompt('Do I like swimming?');
   break;
 }
+console.log(score);
 let travelingCase= prompt('Have I ever travelled before?');
 switch (travelingCase.toLowerCase()){
 case 'yes':
@@ -37,10 +41,12 @@ case 'y':
 case 'no':
 //console.log('Unfortunately.');
   alert('Unfortunately.');
+  score++;
   break;
 case 'n':
 //console.log('Unfortunately.');
   alert('Unfortunately.');
+  score++;
   break;
 default:
   alert('Why! try it! Just guess yes or no ');
@@ -59,24 +65,28 @@ case 'y':
 case 'no':
 //console.log('hooorry!');
   alert('hooorry!');
+  score++;
   break;
 case 'n':
 //console.log('hooorry!');
   alert('hooorry!');
+  score++;
   break;
 default:
-  alert('Why! try it! Just guess yes or no ')
+  alert('Why! try it! Just guess yes or no ');
   myMovies= prompt('Do I like horror movies?');
   break;}
 let myRelations= prompt('Do I like make a big network relations?');
 switch (myRelations.toLowerCase()){
 case 'yes':
 //console.log('Yes!!! such I am so intrested to know about you my new friend');
+  score++;
   alert('Yes!!! such I am so intrested to know about you my new friend');
   break;
 case 'y':
 //console.log('Yes!!! such I am so intrested to know about you my new friend');
   alert('Yes!!! such I am so intrested to know about you my new friend');
+  score++;
   break;
 case 'no':
 //console.log('I hate being lonely ');
@@ -95,10 +105,12 @@ switch (myStatus.toLowerCase()){
 case 'yes':
 //console.log('lol! It is fine It does not matter ');
   alert('lol! It is fine It does not matter ');
+  score++;
   break;
 case 'y':
 //console.log('lol! It is fine It does not matter ');
   alert('lol! It is fine It does not matter ');
+  score++;
   break;
 case 'no':
 //console.log('Oh! Where did you hear this? It is a Rumours ');
@@ -109,9 +121,84 @@ case 'n':
   alert('Oh! Where did you hear this? It is a Rumours');
   break;
 default:
-  alert('Why! try it! Just guess yes or no ')
+  alert('Why! try it! Just guess yes or no ');
   myStatus= prompt('Am I single?');
   break; }
+
+alert( userName+' Why dont we go to the next level?');
+
+let myMonth=11;
+for(let i=0; i<4;i++){
+  let guess= Number(prompt('Quess my Month BD'));
+  if(guess===myMonth){
+    alert('That is correct!');
+    score++;
+    //console.log('correct answer');
+    break;
+  }
+  else if (guess<=6){
+    alert('It is too low,try another number');
+  //console.log('incorrect answer')
+  }
+  else if(6 < guess || guess <=10){
+    alert('You are about to reach! but still low');
+  //console.log('incorrect answer');
+  }
+  else if (guess===12) {
+    alert('Why do not you increase it by one!');
+    //console.log('incorrect answer');
+  }
+  else{
+    alert('Choice a number between 1-12!');}
+  if(i===3){
+    alert('Sorry, you finished your attempts.. The correct answer is 11');
+    //console.log('incorrect answer');
+  }
+}
+let userGuess= prompt('Can you guess my lovely city in the world?');
+let favCity=['london','sydney','paris','tokyo'];
+for(let i=0; i<5;i++){
+  if(userGuess.toLowerCase()==='london'){
+    alert('That is correct!');
+    score++;
+    //console.log('correct answer');
+    break;
+  }
+  else if(userGuess.toLowerCase()==='sydney'){
+    alert('That is correct!');
+    score++;
+    //console.log('correct answer');
+
+    break;
+  }
+  else if(userGuess.toLowerCase()==='paris'){
+    alert('That is correct!');
+    score++;
+    //console.log('correct answer');
+
+    break;
+  }
+  else if(userGuess.toLowerCase()==='tokyo'){
+    alert('That is correct!');
+    score++;
+    //console.log('correct answer');
+
+    break;
+  }
+  else{
+    alert('Try again');
+    userGuess= prompt('Can you guess my lovely city in the world?');
+    //console.log('incorrect answer');
+
+  }
+  if(i===4){
+    alert('Sorry, you finished your attempts.. The correct answer is 11');
+  }
+}
+
+alert('Your score is: '+score+' out of 7');
+
+
 
 alert( userName+' ! Thank you for your time');
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # 201-lab01
+## Synopsis
+
+In this project, I created a basic web browser and make an interactive page with the user by made quiz game.
+
+## Code Example
+
+Show what the library does as concisely as possible, developers should be able to figure out **how** your project solves their problem by looking at the code example. Make sure the API you are showing off is obvious, and that your code is short and concise.
+
+## Motivation
+
+To modify it as possible as I can after each course, by adding a new methods and prpoerties.
+To make my web page more fun, helpful and joyful for the user.
+
+
+## Contributors
+
+To get more Information and details about my project, you can contact me by the following link:
+
+[Github](https://github.com/NadaFraihat)
